da48597430712569f3cebb14dcb858d45780874b
padrigon-lynbert <padrigonlynbert@gmail.com>
2025-05-23 09:59:26 +0800

    add: Procfile

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'portfolio.urls'
+ROOT_URLCONF = 'PORTFOLIO.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portfolio.wsgi.application'
+WSGI_APPLICATION = 'PORTFOLIO.wsgi.application'
 
 
 # Database
